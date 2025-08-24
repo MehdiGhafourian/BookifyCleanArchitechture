a884b01f95c1ec705b1138671f7f46a5d5121854
MehdiGhafourian <mehdi.ghafourian@yahoo.com>
2025-08-24 08:03:00 +0330

    Add Concurrency exception on race condition when persist data into database on saving changes.

--- src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Domain.Abstractions;
+﻿using Bookify.Application.Exceptions;
+using Bookify.Domain.Abstractions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,11 +23,19 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        var result = await base.SaveChangesAsync(cancellationToken);
+        try
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
 
-        await PublishDomainEventAsync();
+            await PublishDomainEventAsync();
 
-        return result;
+            return result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exception occured.", ex);
+        }
+
     }
 
     private async Task PublishDomainEventAsync()
